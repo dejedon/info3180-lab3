5728838c5a79179a02a28f1c326387681ff15cc3
Dejean Whyte <dejeanwhyte@gmail.com>
2024-02-28 23:50:24 -0500

    Uploaded Code

--- app/config.py
+++ app/config.py
@@ -6,4 +6,8 @@ load_dotenv()
 class Config(object):
     """Base Config Object"""
     DEBUG = True
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
+    SECRET_KEY = "Som3$ec5etK*y"
+    MAIL_SERVER = "sandbox.smtp.mailtrap.io"   
+    MAIL_PORT = 465     
+    MAIL_USERNAME = "a3c13cfda3dd08"  
+    MAIL_PASSWORD = "ddfad67786a98b"
--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+from app.forms import ContactForm
+from app import mail
+from flask_mail import Message
 
 ###
 # Routing for your application.
@@ -17,6 +19,36 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+@app.route('/contact/', methods=['GET', 'POST'])
+def contact():
+    """Render the website's contact page."""
+
+    form = ContactForm(request.form)
+
+    if request.method == "POST" and form.validate_on_submit():
+        # Retrieve form data
+        name = form.name.data
+        email = form.email.data
+        subject = form.subject.data
+        message = form.message.data
+
+        # Send email 
+        msg = Message(subject, 
+                sender=(name, email),
+                recipients=["dejeanwhyte@gmail.com"])
+        msg.body = message
+        mail.send(msg)
+
+        # Flash success message and redirect user to home page
+        flash("Form successfully submitted!", "success")
+        return redirect(url_for('home'))
+
+    # Display errors if form not submitted correctly
+    if form.errors:
+        flash("Please fill out all fields appropriately.")
+        flash_errors(form)
+
+    return render_template('contact.html', form=form)
 
 ###
 # The functions below should be applicable to all Flask apps.
